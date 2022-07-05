5514923ee94471d3e8359dff25459eb627c53e94
wahi90 <mdshahed1098@gmail.com>
2022-07-05 15:23:32 +0600

    typescript basics done

--- basics.ts
+++ basics.ts
@@ -11,7 +11,7 @@ const person:{
     Is_married:true,
 }
 
-console.log(person.Is_married)
+// console.log(person.Is_married)
 
 //array 
 // you cannot use map on string Array .
@@ -35,4 +35,44 @@ add = (a: number , b: number , c: string ) =>{
 
 }
 const sub = add(3, 3 , 'false')
-console.log(sub)
+// console.log(sub)
+
+
+//class
+
+class Person {
+ name: string;
+ age: number;
+
+ constructor(n: string, a: number) {
+    
+        this.name= n;
+        this.age = a;
+    
+ }
+ role(){
+    console.log(`${this.name} is a hacker`)
+ }
+}
+
+const person1 = new Person('sahed', 30)
+
+person1.role()
+
+// Generics
+// everyone use <T> as a generics type. Basically generics is a variable . 
+//we can also give <T> as specific type. ex: <T extends object> or <T extends { name: string, age: number}> which means T can only extend the objects value.
+//this is use for accessing object.
+
+function add2<T>(obj: T) {
+ const id = Math.floor(Math.random() * 100)
+ return {...obj , id}
+}
+
+const argument = add2({
+    name:'sahed',
+    age:30
+})
+console.log(argument.age)
+//without generics iam cannot access object without id .
+// with generics i can access anything from objects
